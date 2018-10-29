f2ee7905c45dabbf21a8b993ed59404fbb4ba8c5
Adam Sales <asales@utexas.edu>
2018-10-29 13:14:21 -0500

    added 17, switched to readr

--- makeData.r
+++ makeData.r
@@ -1,17 +1,32 @@
 library(survey)
-setwd('../../data/byYear')
+library(readr)
 
-makeDes <- function(year){
+varNames <- tolower(c('ST','SERIALNO','AGEP','DDRS','DEAR','DEYE','DOUT','DPHY','DRATX','DREM','FDEARP','ESR','SCHL','RAC1P','HISP','SEX','PERNP','PINCP','SSIP','WKHP','WKW','ADJINC','PWGTP','INTP', 'OIP', 'PAP', 'RETP', 'SEMP',  'SSP', 'WAGP',paste0('pwgtp',1:80)))
 
-    print(year)
-    filea <- read.csv(paste0('ss',year,'pusa.csv'))[,c('ST','SERIALNO','AGEP','DDRS','DEAR','DEYE','DOUT','DPHY','DRATX','DREM','FDEARP','ESR','SCHL','RAC1P','HISP','SEX','PERNP','PINCP','SSIP','WKHP','WKW','ADJINC','PWGTP','INTP', 'OIP', 'PAP', 'RETP', 'SEMP',  'SSP', 'WAGP',paste0('pwgtp',1:80))]
-    fileb <- read.csv(paste0('ss',year,'pusb.csv'))[,c('ST','SERIALNO','AGEP','DDRS','DEAR','DEYE','DOUT','DPHY','DRATX','DREM','FDEARP','ESR','SCHL','RAC1P','HISP','SEX','PERNP','PINCP','SSIP','WKHP','WKW','ADJINC','PWGTP','INTP', 'OIP', 'PAP', 'RETP', 'SEMP',  'SSP', 'WAGP',paste0('pwgtp',1:80))]
+makeDes <- function(YR){
 
-    dat <- rbind(filea,fileb)
-    rm(filea,fileb); gc()
+    print(YR)
 
-    hdat <- rbind(read.csv(paste0('ss',year,'husa.csv'))[,c('SERIALNO','TYPE')],
-                  read.csv(paste0('ss',year,'husb.csv'))[,c('SERIALNO','TYPE')])
+    yr <- if(is.integer(YR)){ if(YR<10) paste0('0',YR) else(paste0(YR)) } else YR
+    firstTry <- read_csv(paste0('../../data/byYear/ss',yr,'pusa.csv'), n_max=5)
+    colTypes <- paste(ifelse(tolower(names(firstTry))%in%varNames,'i','-'),collapse='')
+
+    datA <- read_csv(paste0('../../data/byYear/ss',yr,'pusa.csv'),col_types=colTypes)
+    names(datA) <- tolower(names(datA))
+    stopifnot(all.equal(sort(names(datA)),sort(varNames)))
+
+    datB <- read_csv(paste0('../../data/byYear/ss',yr,'pusb.csv'),col_types=colTypes)
+    names(datB) <- tolower(names(datB))
+    dat <- rbind(datA[,varNames],datB[,varNames])
+
+    rm(datA,datB); gc()
+
+    names(dat) <- toupper(names(dat))
+    names(dat)[grep('PWGTP[0-9]+',names(dat))] <- tolower( names(dat)[grep('PWGTP[0-9]+',names(dat))])
+
+
+    hdat <- rbind(read.csv(paste0('../../data/byYear/ss',yr,'husa.csv'))[,c('SERIALNO','TYPE')],
+                  read.csv(paste0('../../data/byYear/ss',yr,'husb.csv'))[,c('SERIALNO','TYPE')])
 
     dat$type <- hdat$TYPE[match(dat$SERIALNO,dat$SERIALNO)]
 
@@ -52,10 +67,10 @@ saveDes <- function(des){
                                      labels=c('25-34','35-44','45-54','55-64'),ordered=TRUE))
 
 
-    save(des,file=paste0('design',year,'.RData'))
+    save(des,file=paste0('../../data/byYear/design',yr,'.RData'))
 }
 
-for(year in c('08','09','10','11','12','13','14','15','16')){
+for(year in c('08','09','10','11','12','13','14','15','16','17')){
     start <- proc.time()
     des <- makeDes(year)
     print('design made')
@@ -71,4 +86,4 @@ for(year in c('08','09','10','11','12','13','14','15','16')){
 
 
 
-setwd('deafCenter/changeOverTime25.34')
+#setwd('deafCenter/changeOverTime25.34')
