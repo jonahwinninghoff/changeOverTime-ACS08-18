0c616d75fd0984bb7d87f030d2d051c2ff9b96a9
adamSales <asales@utexas.edu>
2019-05-02 12:23:30 -0500

    estimate with dplyr (MUCH faster)

--- estByYear.r
+++ estByYear.r
@@ -5,25 +5,65 @@ library(survey)
 
 abvs <- c(HS='hs',Associates='cc',Bachelors='bach')
 
+## est1 <- function(edLev,des,raw=FALSE){
+##     abv <- if(edLev%in%names(abvs)) abvs[edLev] else 'lev'
+##     subsets <- list('AGEP','ageRange','raceEth','SEX',c('SEX','raceEth'))
+##     if(raw) subsets <- c('DEAR','I(AGEP<30)',subsets)
+##     subsets <- lapply(subsets,function(sss) reformulate(if(raw) c('DEAR',sss) else c('DEAR','AGEP',sss)))
+
+##     out <- lapply(subsets, function(x)
+##         merge( svyby(~I(attain>=edLev),by=x,design=des,FUN=svymean,na.rm=TRUE),
+##               as.data.frame(xtabs(x,des$variables,drop=TRUE))))
+
+##     names(out) <- paste0(abv,c(if(raw) c('Tot','25.29','ByAge') else 'Tot',
+##                                'ByAgeCat','Race','Sex','RaceSex'))
+
+##     if(!raw) out[[paste0(abv,'25.29')]] <- subset(out[[paste0(abv,'Tot')]],AGEP<30)
+
+##     out <- lapply(out,function(x) x[,-c(grep('FALSE',colnames(x)),which(colnames(x)=='se1'))])
+
+##     out
+## }
+
 est1 <- function(edLev,des,raw=FALSE){
     abv <- if(edLev%in%names(abvs)) abvs[edLev] else 'lev'
     subsets <- list('AGEP','ageRange','raceEth','SEX',c('SEX','raceEth'))
-    if(raw) subsets <- c('DEAR','I(AGEP<30)',subsets)
-    subsets <- lapply(subsets,function(sss) reformulate(if(raw) c('DEAR',sss) else c('DEAR','AGEP',sss)))
 
-    out <- lapply(subsets, function(x)
-        merge( svyby(~I(attain>=edLev),by=x,design=des,FUN=svymean,na.rm=TRUE),
-              as.data.frame(xtabs(x,des$variables,drop=TRUE))))
+    dat <- des$variables[,c('DEAR','attain',unique(unlist(subsets)))]
+    dat$pwgtp <- des$pweights
+    dat <- cbind(dat,des$repweights)
+    dat[[abv]] <- dat$attain>=edLev
+
+    if(raw){
+      dat$ya <- dat$AGEP<30
+      subsets <- c('DEAR','ya',subsets)
+    }
+    subsets <- lapply(subsets,function(sss)
+      if(raw) unique(c('DEAR',sss)) else unique(c('DEAR','AGEP',sss)))
+
+    outfun <- function(sss){
+      out <- dat%>%group_by(!!! syms(sss))%>%do(x=factorProps(abv,.))
+      ests <- out$x
+      out$x <- NULL
+      out <- bind_cols(out,do.call('bind_rows',ests))
+      out <- out[,-grep('FALSE',colnames(out))]
+      out <- rename(out,`I(attain >= edLev)TRUE`=`% TRUE`,se2=`TRUE SE`,Freq=n)
+      out
+    }
+
+
+    out <- lapply(subsets, outfun)
 
     names(out) <- paste0(abv,c(if(raw) c('Tot','25.29','ByAge') else 'Tot',
                                'ByAgeCat','Race','Sex','RaceSex'))
 
     if(!raw) out[[paste0(abv,'25.29')]] <- subset(out[[paste0(abv,'Tot')]],AGEP<30)
 
-    out <- lapply(out,function(x) x[,-c(grep('FALSE',colnames(x)),which(colnames(x)=='se1'))])
-
     out
 }
+
+
+
 
 sampSize <- function(des){
     out <- list()
@@ -41,9 +81,9 @@ sampSize <- function(des){
     out
 }
 
-yearInfo <- function(year){
+yearInfo <- function(year,des){
     print(year)
-    load(paste0('../../data/byYear/design',year,'.RData'))
+    if(missing(des)) load(paste0('../../data/byYear/design',year,'.RData'))
     des <- subset(des,AGEP>24 & AGEP<65) ## changed this to change age range
 #    des <- update(des,raceEth=ifelse(raceEth%in%c('American Indian','Asian/PacIsl'),'Other',raceEth))
 
@@ -59,31 +99,31 @@ yearInfo <- function(year){
 }
 
 
-library(parallel)
-cl <- makeCluster(4)
-clusterEvalQ(cl,library(survey))
-clusterExport(cl,c('abvs','est1','sampSize'))
+## library(parallel)
+## cl <- makeCluster(2)
+## clusterEvalQ(cl,library(survey))
+## clusterExport(cl,c('abvs','est1','sampSize'))
 
-years <- c('08','09','10','11','12','13','14','15','16','17')
-overTimeTot <- parLapply(cl,years,yearInfo)
-names(overTimeTot) <- years
-save(overTimeTot,file='output/estsOverTimeTot.RData')
+## years <- c('08','09','10','11','12','13','14','15','16','17')
+## overTimeTot <- parLapply(cl,years,yearInfo)
+## names(overTimeTot) <- years
+##
+
+## stopCluster(cl)
+
+## ## overTimeTot <- lapply(overTimeTot, function(x){
+## ##     sscc <- x$outSS[grep('hs',names(x$outSS))]
+## ##     names(sscc) <- gsub('hs','cc',names(sscc))
+## ##     x$outSS <- c(x$outSS,sscc)
+## ##     with(x,list(outRaw=lapply(names(outRaw),function(nn) merge(outRaw[[nn]],as.data.frame(outSS[[nn]]))),
+## ##                 outAdj=
 
 
+## overTime <- sapply(overTimeTot,function(x) x$outRaw,simplify=FALSE,USE.NAMES=TRUE)
+## overTimeAge <- sapply(overTimeTot,function(x) x$outAdj,simplify=FALSE,USE.NAMES=TRUE)
+## sampleSizes <- sapply(overTimeTot,function(x) x$outSS,simplify=FALSE,USE.NAMES=TRUE)
 
-## overTimeTot <- lapply(overTimeTot, function(x){
-##     sscc <- x$outSS[grep('hs',names(x$outSS))]
-##     names(sscc) <- gsub('hs','cc',names(sscc))
-##     x$outSS <- c(x$outSS,sscc)
-##     with(x,list(outRaw=lapply(names(outRaw),function(nn) merge(outRaw[[nn]],as.data.frame(outSS[[nn]]))),
-##                 outAdj=
-
-
-overTime <- sapply(overTimeTot,function(x) x$outRaw,simplify=FALSE,USE.NAMES=TRUE)
-overTimeAge <- sapply(overTimeTot,function(x) x$outAdj,simplify=FALSE,USE.NAMES=TRUE)
-sampleSizes <- sapply(overTimeTot,function(x) x$outSS,simplify=FALSE,USE.NAMES=TRUE)
-
-save(overTime,overTimeAge,sampleSizes,file='output/estByYear.RData')
+## save(overTime,overTimeAge,sampleSizes,file='output/estByYear.RData')
 
 ## nyear <- length(years)
 ## nAnal <- length(overTime[[1]])
